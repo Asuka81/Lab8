0c25d93033c6258f2059b69285f839e6691299f6
Asuka81 <71114413+Asuka81@users.noreply.github.com>
2022-10-11 15:37:07 +0700

    Add files via upload

---
+++ api-gateway/src/test/java/com/sop/chapter6/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,14 @@
+package com.sop.chapter6.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ test-service/src/test/java/com/sop/chapter6/testservice/TestServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sop.chapter6.testservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@EnableDiscoveryClient
+@SpringBootTest
+class TestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
